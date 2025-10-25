3e22b05d8b0c5a2a1b04d2635de654f4bdf27145
pvlkurial <pavelkurial0@gmail.com>
2025-10-25 22:39:04 +0200

    Add tier to timegoalmappacktrack

--- internal/models/timegoalMappackTrack.go
+++ internal/models/timegoalMappackTrack.go
@@ -4,9 +4,10 @@ import "time"
 
 // "Instance" of Time goal type for a track in a mappack
 type TimeGoalMappackTrack struct {
-	ID             int `gorm:"primaryKey"`
-	TimeGoalID     int `json:"time_goal_id"`
-	MappackTrackID int `json:"mappack_track_id"`
-	Time           int `json:"time"`
+	ID             int    `gorm:"primaryKey"`
+	TimeGoalID     int    `json:"time_goal_id"`
+	MappackTrackID int    `json:"mappack_track_id"`
+	Time           int    `json:"time"`
+	Tier           string `json:"tier"`
 	UpdatedAt      time.Time
 }
